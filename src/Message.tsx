a091757e9eed9f1dc370ab002fd5dcd311181565
paulnyabaro <paulnyabaro1@gmail.com>
2023-07-01 09:49:59 +0300

    Created message component

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,7 @@
 function Message() {
     // JSX: JavaScript XML 
-    return <h1>Hello the world of React!</h1>
+    const name = 'Mosh';
+    return <h1>Hello the world of React with {name}!</h1>
 }
 
 export default Message;
