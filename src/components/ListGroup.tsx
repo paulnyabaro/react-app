4ca2a80bbec55dc0b3a765eaee1f0b423c204c5c
paulnyabaro <paulnyabaro1@gmail.com>
2023-07-01 10:24:50 +0300

    Created components folder

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,14 @@
+function ListGroup() {
+    return (
+        <div><h1>List Group</h1>
+        <ul className="list-group">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">Item three</li>
+            <li className="list-group-item">A fourth item</li>
+        </ul>
+        </div>
+    );
+}
+
+export default ListGroup;
