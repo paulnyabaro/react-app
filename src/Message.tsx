715d1fca699020c9bec106eed6dc693aed6be79e
paulnyabaro <paulnyabaro1@gmail.com>
2023-07-01 09:51:57 +0300

    Using functions in React components

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,9 @@
 function Message() {
     // JSX: JavaScript XML 
     const name = 'Mosh';
-    return <h1>Hello the world of React with {name}!</h1>
+    if (name)
+        return <h1>Hello the World of React with {name}!</h1>;
+    return <h1>Hello World!</h1>
 }
 
 export default Message;
