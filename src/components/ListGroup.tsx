8e2f414dd82b49f347205b5a37e8adbbea5812dd
paulnyabaro <paulnyabaro1@gmail.com>
2023-07-02 21:04:07 +0300

    Using dynamic items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,14 +6,13 @@ function ListGroup() {
         'London',
         'Paris'
     ];
+
+    
     return (
         <>
         <h1>List Group</h1>
         <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">Item three</li>
-            <li className="list-group-item">A fourth item</li>
+            {items.map((item) => <li className="list-group-item">{item}</li>)}
         </ul>
         </>
     );
