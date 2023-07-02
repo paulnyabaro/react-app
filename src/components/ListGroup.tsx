c86b66fed3cf1d98214e6e6aac6b3d27591f913b
paulnyabaro <paulnyabaro1@gmail.com>
2023-07-02 21:29:48 +0300

    Created the navbar component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -8,12 +8,13 @@ function ListGroup() {
     ];
     // items = []
 
-    if (items.length === 0)
-        return <div className="alert alert-primary">No items available yet</div>
+    // if (items.length === 0)
+    //     return <div className="alert alert-primary">No items available yet</div>
     
     return (
         <>
         <h1>List Group</h1>
+        {items.length === 0 && <div className="alert alert-primary">No items available yet</div>}
         <ul className="list-group">
             {items.map((item) => <li key={item} className="list-group-item">{item}</li>)}
         </ul>
