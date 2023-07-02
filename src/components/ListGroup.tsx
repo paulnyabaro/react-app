d8fe4ee1f95f2f5220bb6b26d74ed29f14a70250
paulnyabaro <paulnyabaro1@gmail.com>
2023-07-02 21:09:39 +0300

    Added a unique key

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -12,7 +12,7 @@ function ListGroup() {
         <>
         <h1>List Group</h1>
         <ul className="list-group">
-            {items.map((item) => <li className="list-group-item">{item}</li>)}
+            {items.map((item) => <li key={item} className="list-group-item">{item}</li>)}
         </ul>
         </>
     );
