2b86c4b8fecd1d0cf4ad0c7844a65be642d20f8b
paulnyabaro <paulnyabaro1@gmail.com>
2023-07-04 22:10:39 +0300

    Added alert component

---
+++ src/components/Alert.tsx
@@ -0,0 +1,13 @@
+interface Props{
+    text: string;
+}
+
+const Alert = ({ text }: Props) => {
+    return (
+        <div className="alert alert-primary">
+            {text}
+        </div>
+    )
+}
+
+export default Alert
